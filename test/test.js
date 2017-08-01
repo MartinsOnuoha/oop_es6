8524946087263d91794fba9f8c46a73e0710d6cc
MartinsOnuoha <martinsvictor.nd@gmail.com>
2017-08-01 20:16:51 +0100

    fixed testcase3

--- test/test.js
+++ test/test.js
@@ -2,12 +2,16 @@ const assert = require('chai').assert;
 const BankAccount = require('../src/bank.js').BankAccount;
 const CurrenAccount = require('../src/bank.js').CurrentAccount;
 
+// initialise variables
 let account1 = new BankAccount('Martins', 123);
 let current1 = new CurrenAccount('Martins', 123);
+let amount = 500;
+let success = "Transaction Successful! Balance is: ";
+let failure = "Transaction Unsuccessful! Balance is: ";
 
 describe('Accounts', function() {
 
-    describe("checks user account balance", function() {
+    describe("checks user account", function() {
         it('should return current balance', function() {
             assert.equal(account1.getBalance(), account1.balance);
         });
@@ -15,5 +19,14 @@ describe('Accounts', function() {
         it('should return user details', function() {
             assert.equal(account1.getDetail(123), account1.name + ', ' + account1.balance);
         });
+
     });
-});
+
+      describe("successful transaction", function(){
+          it('should deduct amount from balance', function() {
+          assert.equal(account1.withdraw(account1.name, amount, account1.pin), success + account1.balance);
+        });
+      });
+
+
+    });
