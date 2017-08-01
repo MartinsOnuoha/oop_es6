05d68f9bf3c73ad5507859d25bc21023ccb4e8c6
MartinsOnuoha <martinsvictor.nd@gmail.com>
2017-08-01 15:21:16 +0100

    Implements Encapsulation

--- src/bank.js
+++ src/bank.js
@@ -1,16 +1,35 @@
 class BankAccount {
-    constructor(firstName, lastName, pin, account_balance=1500) {
-        this.firstname = firstName;
-        this.lastname = lastName;
+    constructor(lastName, pin, account_balance=1500) {
+        this.name = lastName;
         this.pin = pin;
         this.balance = account_balance;
     }
 
-    static checkBalance(pin) {
+   
+
+    getBalance() {
         return this.balance;
     }
-    static accountDetail(pin) {
-        details = [this.firstname, this.lastname, this.balance];
-        return details;
+     /* Encapsulation */
+    getDetail(pin) {
+        details = [this.name, this.balance];
+        if(pin === this.pin){
+            return details;
+        }
+    }
+    
+    
+    withdraw(name, amount, pin) {
+        
+        if (amount < 0){
+          return "Invalid Amount!"
+        }
+        else if(pin === this.pin && name === this.name && amount < this.balance){
+          this.balance -= amount;
+          return "Transaction Successful! Balance is: " + this.balance;
+        }
+        else {
+          return "Transaction Unsuccessful! Balance is: " + this.balance;
+        }       
     }
 }
